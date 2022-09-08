86d48459c0f9e8ae51a6fe71e9a4d050b785ef65
Pavel <aivellinsom@gmail.com>
2022-09-08 20:45:56 +0400

    Add routes for watch and forum

--- watch_and_chat/watch_and_chat/settings.py
+++ watch_and_chat/watch_and_chat/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "watch.apps.WatchConfig",
+    "forum.apps.ForumConfig",
 ]
 
 MIDDLEWARE = [
